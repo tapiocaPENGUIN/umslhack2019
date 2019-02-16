6c3ec000c4560fd1f19691bb3388965f88355bcb
Brunsvold <ebyrd@es107m03.dhcp.umsl.edu>
2019-02-16 10:21:25 -0600

    Connected to DB and able to update information on the firestore

---
+++ MyApplication/app/src/main/java/com/example/ebyrd/myapplication/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.ebyrd.myapplication;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        test();
+    }
+
+    public void test(){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "ada");
+        user.put("last", "Lovelace");
+
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        System.out.print("Success");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        System.out.print("Failure");
+                    }
+                });
+
+    }
+}
